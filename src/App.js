9e68cdf9e6ceb9cbf7fc4443f7b84370a30486af
Danicode <ariyoesther2009@gmail.com>
2024-05-18 21:05:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+    
+  const [name, setname] = useState("")
+  const [todo, setTodo] = useState("")
+  const [alltodo, setalltodo] = useState([])
+
+
+
+  const add = ()=>{
+    let todos = {
+      names:name,
+      todod:todo
+    } 
+      setalltodo([...alltodo,todos])
+      console.log(alltodo);
+      // console.log(todos);
+  }
+
+  const deleteitem = (index) => {
+    console.log(index);
+    // Create a copy of the array with the item at the given index removed
+    setalltodo(()=>{
+      let updatedTodo = [...alltodo];
+      console.log(updatedTodo);
+        updatedTodo.splice(index,1)
+    // Return the updated array or do something else with it
+        return updatedTodo;
+
+    })
+};
+
+
+    const edit = ()=> {
+
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className='sub-app'>
+        <h2 className='myh2'>Todo List ❤️</h2>
+        <div>
+          <input className='nameinput' onChange={(e)=> setname(e.target.value)} type="text" placeholder='Enter your name'/>
+         </div>
+        <div>
+          <input className='nameinput' onChange={(e)=>setTodo(e.target.value)} type="text" placeholder='Todo....'/>
+         </div>
+         <button onClick={add} className='button'>Add</button>
+
+          <div className='bigtodolist'>
+          {alltodo.map((el,index) => (
+          <div key={index} className='todolist'>
+            <p>{el.names}</p>
+            <h3>{el.todod}</h3>
+            <div>
+            <button id='delete' onClick={deleteitem} ><i className="fa fa-trash"></i></button>
+            <button onClick={edit}>Edit</button>
+            </div>
+          </div>
+         ))}
+          </div>
+       </div>
     </div>
   );
 }
