0a6b1faf0242f47a71503059ccb6b3dc3db9f8c7
Danicode <ariyoesther2009@gmail.com>
2024-05-21 13:42:14 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
 const App = () => {
     
   const [name, setname] = useState("")
   const [todo, setTodo] = useState("")
-  const [alltodo, setalltodo] = useState([])
-
+  const [alltodo, setalltodo] = useState( JSON.parse(localStorage.getItem("todolist"))|| [])
+  const [onetodo, setonetodo] = useState({})
+  const [index, setindex] = useState("")
+ 
+  useEffect(() => {
+    
+    localStorage.setItem("todolist",JSON.stringify(alltodo))
+  },[alltodo])
+  
 
 
   const add = ()=>{
@@ -26,6 +33,8 @@ const App = () => {
       let updatedTodo = [...alltodo];
       console.log(updatedTodo);
         updatedTodo.splice(index,1)
+    localStorage.setItem("todolist",JSON.stringify(alltodo))
+
     // Return the updated array or do something else with it
         return updatedTodo;
 
@@ -33,7 +42,16 @@ const App = () => {
 };
 
 
-    const edit = ()=> {
+    const edit = (index,el)=> {
+      console.log(index,el);
+      setindex(index)
+      setonetodo({...el})
+    }
+    const save = ()=>{
+      alltodo[index] = onetodo
+      console.log(onetodo);
+      setalltodo([...alltodo])
+    localStorage.setItem("todolist",JSON.stringify(alltodo))
 
     }
 
@@ -53,15 +71,37 @@ const App = () => {
           {alltodo.map((el,index) => (
           <div key={index} className='todolist'>
             <p>{el.names}</p>
-            <h3>{el.todod}</h3>
+            <h4>{el.todod}</h4>
             <div>
-            <button id='delete' onClick={deleteitem} ><i className="fa fa-trash"></i></button>
-            <button onClick={edit}>Edit</button>
+            <button id='delete'  class="btn btn-danger" onClick={deleteitem} ><i className="fa fa-trash"></i></button>
+            <button  class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"onClick={()=>edit(index,el)}>Edit</button>
             </div>
           </div>
          ))}
+
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Todo</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <input value={onetodo.names} onChange={(e)=>setonetodo({...onetodo,names:e.target.value})} className='form-control mt-3' type="text" />
+        <input value={onetodo.todod} onChange={(e)=>setonetodo({...onetodo,todod:e.target.value})} className='form-control mt-3' type="text" />
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" onClick={save} class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
           </div>
        </div>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'font-awesome/css/font-awesome.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/js/bootstrap.bundle.js"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
